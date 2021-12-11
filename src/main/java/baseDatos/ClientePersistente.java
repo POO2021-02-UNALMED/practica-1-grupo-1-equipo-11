014dc686bbb0a18cfdf7340169d2c1f19ef53359
JuandaBula <92651896+JuandaBula@users.noreply.github.com>
2021-12-11 18:12:47 -0500

    Update ClientePersistente

--- src/main/java/baseDatos/ClientePersistente.java
+++ src/main/java/baseDatos/ClientePersistente.java
@@ -2,8 +2,17 @@ package baseDatos;
 
 import gestorAplicacion.dominio.Cliente;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.List;
 
+/**
+ * Clase que implementa la interfaz Persistente para manejar la escritura y lectura de clientes en archivos.
+ *
+ * @author Juan Daniel Bula, Leidy Daniela Alzate, Oscar Andres Usuga
+ */
 public class ClientePersistente implements Persistente<Cliente> {
 
     @Override
@@ -14,5 +23,19 @@ public class ClientePersistente implements Persistente<Cliente> {
     @Override
     public void guardar(Cliente cliente) {
 
+    }
+
+    @Override
+    public Cliente leerUno(String identificador) {
+        FileInputStream fis;
+        try {
+            String ruta = String.format("/src/main/java/baseDatos/temp/clientes/%s.txt", identificador);
+            fis = new FileInputStream(System.getProperty("user.dir") + ruta);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            return (Cliente) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
