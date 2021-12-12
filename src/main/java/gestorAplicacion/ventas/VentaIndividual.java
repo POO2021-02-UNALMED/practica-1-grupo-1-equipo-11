8ed5b1f3d9ec5985953f4862d86ebb655f07f25c
ldalzate <ldalzatef@unal.edu.co>
2021-12-11 22:52:24 -0500

    Implementar Serializable en VentaIndividual

--- src/main/java/gestorAplicacion/ventas/VentaIndividual.java
+++ src/main/java/gestorAplicacion/ventas/VentaIndividual.java
@@ -1,4 +1,47 @@
 package gestorAplicacion.ventas;
 
-public class VentaIndividual {
+import gestorAplicacion.dominio.Cliente;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Clase que representa una venta individual en el sistema.
+ *
+ * @author Juan Daniel Bula, Leidy Daniela Alzate, Oscar Andres Usuga
+ */
+public class VentaIndividual implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 4L;
+
+    private String codigoVenta;
+    private Cliente cliente;
+    private List<Producto> productos;
+    private String estado;
+
+    public VentaIndividual(String codigo, Cliente cliente, List<Producto> productos) {
+        this.codigoVenta = codigo;
+        this.cliente = cliente;
+        this.productos = productos;
+        this.estado = "PENDIENTE";
+    }
+
+    public void venderProductos() {
+        this.estado = "FINALIZADA";
+        this.productos.forEach(Producto::venderProducto);
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public String getCodigoVenta() {
+        return codigoVenta;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
 }
