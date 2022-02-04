a3e56e0ca71646adb5f6fa02dcbc0efd36426717
Oscar Andrés Úsuga Nanclares <oscarandresu@gmail.com>
2022-02-04 01:19:14 -0500

    Adding products

--- python/basedatos/producto_persistente.py
+++ python/basedatos/producto_persistente.py
@@ -8,7 +8,7 @@ class ProductoPersistente:
         self._path = f'{os.path.dirname(os.path.abspath(__file__))}/temp/productos'
 
     def guardar(self, producto):
-        file = f'{self._path}/{producto.get_codigo()}'
+        file = f'{self._path}/{producto.getCodigo()}'
         try:
             with open(file, 'wb') as f:
                 pickle.dump(producto, f, pickle.HIGHEST_PROTOCOL)
