48b6a64e35de1e83a819a41b2dd519ddb3b6412d
Leidy Daniela Alzate Florez <92174354+ldalzate@users.noreply.github.com>
2022-02-03 23:21:13 -0500

    Actualizar ventana_inicio

--- python/uimain/ventana_inicio.py
+++ python/uimain/ventana_inicio.py
@@ -29,6 +29,53 @@ def inicioCrm():
     boton_salida = Button(crm_ppal, text = 'Volver a ventana principal', width = "50", height = "10", font="Helvetica 18 bold", command = ventanaInicio)
     boton_salida.pack()
 
+# Eventos del ratón
+def show_names(e):
+    descripcion.pack_forget()
+    leidy.pack()
+    oscar.pack()
+    juan.pack()
+    leidy.bind("<Button-1>", show_leidy)
+    oscar.bind("<Button-1>", show_oscar)
+    juan.bind("<Button-1>", show_juan)
+
+def show_leidy(e):
+    global descripcion
+    oscar.pack_forget()
+    juan.pack_forget()
+
+    descripcion = Label(master=frame_P5, text='Estudiante de matemáticas', font="Helvetica 12")
+    descripcion.pack()
+    leidy.bind("<Button-1>", show_names)
+
+def show_oscar(e):
+    global descripcion
+    leidy.pack_forget()
+    juan.pack_forget()
+
+    descripcion = Label(master=frame_P5, text="Estudiante de Ciencias de la Computacion", font="Helvetica 12")
+    descripcion.pack()
+    oscar.bind("<Button-1>", show_names)
+
+def show_juan(e):
+    global descripcion
+    leidy.pack_forget()
+    oscar.pack_forget()
+
+    descripcion = Label(master=frame_P5, text="Estudiante de estadística. \n Apasionado por la ciencia de datos y el fútbol", font="Helvetica 12")
+    descripcion.pack()
+    juan.bind("<Button-1>", show_names)
+
+
+# ------------------------------------------------
+# ----------------  Barra de Menú ----------------
+menubar = Menu(window)
+menu1 = Menu(menubar)
+menubar.add_cascade(menu=menu1, label='Inicio', command=evento)
+menu1.add_command(label="Nosotros", command=info)
+menu1.add_command(label="Salir de la aplicación", command=evento)
+window['menu'] = menubar
+
 
 # -------------------------------------------------
 # --------------- Ventana de inicio ---------------
@@ -72,10 +119,36 @@ oscar.pack()
 juan = Label(master=frame_P5, text="Juan Daniel", font="Helvetica 16")
 juan.pack()
 
+# Control del cursor
+leidy.bind("<Button-1>", show_leidy)
+oscar.bind("<Button-1>", show_oscar)
+juan.bind("<Button-1>", show_juan)
+
 # Frame anidado P6
 frame_P6 = Frame(master=frame_P2, width=450, height=500, borderwidth=2, relief="groove")
 frame_P6.pack(fill="both", expand=True, padx=5, pady=5)
 #label_photo = Label(frame_P6)
 
+
+# -----------------------------------------------------
+# ----------- Posicionamiento Grid de fotos -----------
+# Foto Leidy
+label_leidy = Label(master=frame_P6)
+photo_leidy = PhotoImage(file=r'./photos/leidy.png')
+photo_leidy = photo_leidy.subsample(3)
+label_leidy['image'] = photo_leidy
+label_leidy.grid(row=0, column=0)
+# Foto Oscar
+label_oscar = Label(master=frame_P6)
+photo_oscar = PhotoImage(file=r'./photos/oscar.png')
+photo_oscar = photo_oscar.subsample(2)
+label_oscar['image'] = photo_oscar
+label_oscar.grid(row=0, column=1)
+# Foto Juan Daniel
+label_juan = Label(master=frame_P6)
+photo_juan = PhotoImage(file=r'./photos/juandaniel.png')
+photo_juan = photo_juan.subsample(4)
+label_juan['image'] = photo_juan
+label_juan.grid(row=0, column=3)
 
 window.mainloop()
